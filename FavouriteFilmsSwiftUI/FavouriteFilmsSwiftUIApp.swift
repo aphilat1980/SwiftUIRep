17e9c200f3672e309dc3ce17f4ba23d457c7d033
Filatov Alexander <aphilat@mail.ru>
2023-09-10 21:24:34 +0500

    Initial Commit

---
+++ FavouriteFilmsSwiftUI/FavouriteFilmsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavouriteFilmsSwiftUIApp.swift
+//  FavouriteFilmsSwiftUI
+//
+//  Created by Александр Филатов on 10.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct FavouriteFilmsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
