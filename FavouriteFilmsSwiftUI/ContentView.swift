adefd1980b07356bd40a0d739cf3f66cf9022412
Filatov Alexander <aphilat@mail.ru>
2023-09-12 21:55:16 +0500

    DZ2SwiftUI

--- FavouriteFilmsSwiftUI/ContentView.swift
+++ FavouriteFilmsSwiftUI/ContentView.swift
@@ -8,11 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @AppStorage("TITLE_KEY") var titleOn = false
+    @AppStorage("ROW_HEIGHT_KEY") var rowHeight = 60.0
+    
+    //@State private var titleOn: Bool = true
+    
     var body: some View {
         
         TabView {
             
-            InfoView()
+            InfoView(titleOn: $titleOn, rowHeight: $rowHeight)
                 .tabItem {
                     Label("Info View", systemImage: "list.and.film")
                 }
@@ -20,7 +26,7 @@ struct ContentView: View {
                 .tabItem {
                     Label("HelloView", systemImage: "hand.raised.fill")
                 }
-            SettingView()
+            SettingView(titleOn: $titleOn, rowHeight: $rowHeight)
                 .tabItem {
                     Label("Settings", systemImage: "gearshape.fill")
                 }
